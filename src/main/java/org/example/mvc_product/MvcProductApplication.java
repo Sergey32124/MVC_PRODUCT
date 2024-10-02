bb692f7bcaab7e6c87b5c22afd8ff03595890168
Sergey12432 <tchuxin201@yandex.ru>
2024-10-02 19:03:26 +0300

    Initial commit

---
+++ src/main/java/org/example/mvc_product/MvcProductApplication.java
@@ -0,0 +1,13 @@
+package org.example.mvc_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/mvc_product/MvcProductApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mvc_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
